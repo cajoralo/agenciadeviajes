645b300f3cc9a32de96be094c5980fb3e8987bb1
cajoralo <cjaguilera@tes.edu.ec>
2025-12-02 17:28:27 -0500

    act

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,17 +1,39 @@
-
 package model;
 
-
-    public class Usuario {
+public class Usuario {
+    
     private int id;
-    private String usuario;
+    private String username;
+    private String password;
+    private String email;
+    private String nombre;
+    private String apellido;
 
+    // --- ID ---
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
-    public String getUsuario() { return usuario; }
-    public void setUsuario(String usuario) { this.usuario = usuario; }
+    // --- USERNAME ---
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    // --- PASSWORD ---
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    // --- EMAIL ---
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    // --- NOMBRE ---
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    // --- APELLIDO ---
+    public String getApellido() { return apellido; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
 }
+
 
    
 
